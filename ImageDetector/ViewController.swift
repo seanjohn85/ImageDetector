d5ac566caf179a28ae9a3fcbd065db3208200db6
John <johnkenny1985@gamil.com>
2017-10-01 23:04:51 +0100

    GoogLeNetPlaces Model CoreML iOS sample app
    My first core ml iOS application… uses the GoogLeNetPlaces Model  to
    detect images
    
    I will build on this app to allow users to upload images from their
    camera to examine against the model.
    
    This model is small with a dataset of 250

--- ImageDetector/ViewController.swift
+++ ImageDetector/ViewController.swift
@@ -7,8 +7,14 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet var result: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,5 +27,66 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func detectBtns(_ sender: Any) {
+        //get the button pressed
+        if let button = sender as? UIButton{
+            //get the tag to identify the button pressed
+            let tag = button.tag
+            
+            //get the image using the tag as images are named to match the tag 0 -5
+            guard let image = UIImage(named: "\(tag)") else {
+                return
+            }
+            getImage(image: image)
+            print (tag)
+            
+            
+        }
+    }
+    
+    //get the image by name
+    
+    func getImage(image : UIImage){
+        result.text = "Please wait.."
+        //set up the model
+        guard let model = try? VNCoreMLModel(for: GoogLeNetPlaces().model) else{
+            return
+        }
+        //create a vision request
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else{
+                return
+            }
+            //examine convidence level
+            for result in results{
+                print("\(result.identifier) and confidence level \(result.confidence * 100)")
+            }
+            let firstResult = results.first
+            print("here i am")
+            //update text lable when async thread is complete
+            DispatchQueue.main.async {
+                self.result.text =  firstResult?.identifier
+            }
+            
+        }
+        guard let ciImage = CIImage(image: image) else {
+            return
+        }
+        
+        //run the google google model here
+        
+        let handler = VNImageRequestHandler(ciImage: ciImage, options: [ : ])
+        
+        //create a bg thread
+        DispatchQueue.global().async {
+            do{
+                try handler.perform([request])
+                
+            }catch{
+                print(error)
+            }
+        }
+    }
+    
 }
 
